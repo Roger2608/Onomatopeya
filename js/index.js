80685c5baa2a7c5f4b47432bf6d3b0652635b1c7
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2022-11-22 15:26:45 -0500

    modify styles

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 document.querySelectorAll(".card").forEach(card => {
     card.addEventListener("click", function (event) {
         event.preventDefault();
-        console.log(event);
         let e = event.path.filter(a => a.className == 'card');
         document.getElementById('animal').innerText = e[0].innerText.split('-')[0].trim();
-        console.log(e[0].innerText.split('-')[0].trim());
         Array.from(e[0].children).filter(a => a.nodeName == 'AUDIO' || a.className == 'text')
             .forEach(e => {
                 if (e.nodeName == 'AUDIO') {
@@ -18,7 +16,7 @@ document.querySelectorAll(".card").forEach(card => {
         document.getElementById('ninaArriba').style = 'display:none';
         document.getElementById('text_Animal').style = 'display:block';
         document.getElementById('text_inicial').style = 'display:none';
-        document.getElementById('mostradorContainer').style = 'position: relative';
+        // document.getElementById('mostradorContainer').style = 'position: relative';
     });
 });
 
